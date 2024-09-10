1f9e36b91fccb44c9beb64d7e5512b166dbaf52f
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2024-09-10 11:34:58 +0200

    Added Tests project
    Added the basic structure for the Unit tests

---
+++ Tests/DefaultTest.cs
@@ -0,0 +1,16 @@
+﻿using ExotischNederland;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class DefaultTest
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f47f992-ae9b-427b-8ffd-2f7a58c915ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
