1a3024c0ad02028fe5fc00f80c2ca94475bc2700
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2024-10-25 22:23:39 +0200

    Update SQLDAL.cs
    Fixed bug where reader would stay open before model would be loaded with relationships.
    Return id 0 when no identifier is present on the table.
    Added method for deleting records with a where clause instead of an Id.
    Removed table name parameter from select function

--- ExotischNederland/DAL/SQLDAL.cs
+++ ExotischNederland/DAL/SQLDAL.cs
@@ -35,6 +35,9 @@ namespace ExotischNederland.DAL
                             var values = new Dictionary<string, object>();
                             for (int i = 0; i < reader.FieldCount; i++)
                                 values.Add(reader.GetName(i), reader.GetValue(i));
+
+                            reader.Close();
+                            connection.Close();
                             return (T)Activator.CreateInstance(typeof(T), values);
                         }
                         return default(T);
@@ -59,7 +62,15 @@ namespace ExotischNederland.DAL
                     }
                     command.ExecuteNonQuery();
                     command.CommandText = "SELECT CAST(@@Identity as INT);";
-                    return (int)command.ExecuteScalar();
+                    try
+                    {
+                        object result = (int)command.ExecuteScalar();
+                        return (int)result;
+                    }
+                    catch (Exception e)
+                    {
+                        return 0;
+                    }
                 }
             }
         }
@@ -82,7 +93,7 @@ namespace ExotischNederland.DAL
             }
         }
 
-        // Method to delete a row in the table
+        // Method to delete a row in the table by Id
         public void Delete(string _table, int _id)
         {
             var query = new QueryBuilder(_table).Delete().Where("Id", "=", _id.ToString()).Build();
@@ -96,10 +107,27 @@ namespace ExotischNederland.DAL
             }
         }
 
-        // Method to select all rows from a table
-        public List<T> Select<T>(string _table, Func<QueryBuilder, QueryBuilder> _queryBuilder = null)
+        // Method to delete a row in the table by WHERE clause
+        public void Delete(string _table, Func<QueryBuilder, QueryBuilder> _queryBuilder = null)
         {
-            var queryBuilder = new QueryBuilder(_table).Select();
+            if (_queryBuilder == null) throw new ArgumentNullException("QueryBuilder cannot be null when deleting rows by WHERE clause.");
+
+            var query = _queryBuilder(new QueryBuilder(_table).Delete()).Build();
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Method to select all rows from a table
+        public List<T> Select<T>(Func<QueryBuilder, QueryBuilder> _queryBuilder = null)
+        {
+            string table = typeof(T).Name;
+            var queryBuilder = new QueryBuilder(table).Select();
             var query = _queryBuilder != null ? _queryBuilder(queryBuilder).Build() : queryBuilder.Build();
             List<T> results = new List<T>();
 
